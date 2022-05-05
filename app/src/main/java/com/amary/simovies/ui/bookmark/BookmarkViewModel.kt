315b642e3e915b9e3ea3d653e071f55fd981b3fc
Taufik Amaryansyah <taufik.amary@gmail.com>
2022-05-05 07:26:39 +0700

    feat: setup color & font

--- app/src/main/java/com/amary/simovies/ui/bookmark/BookmarkViewModel.kt
+++ app/src/main/java/com/amary/simovies/ui/bookmark/BookmarkViewModel.kt
@@ -1,13 +1,13 @@
-package com.amary.simovies.ui.notifications
+package com.amary.simovies.ui.bookmark
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class BookmarkViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }

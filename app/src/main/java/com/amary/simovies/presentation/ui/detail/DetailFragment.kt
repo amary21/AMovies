3ae5c724eaa12be0c0b1b57ac40d8619e4b111f5
Taufik Amaryansyah <taufik.amary@gmail.com>
2022-05-07 09:02:50 +0700

    feat: add layouting detail

---
+++ app/src/main/java/com/amary/simovies/presentation/ui/detail/DetailFragment.kt
@@ -0,0 +1,13 @@
+package com.amary.simovies.presentation.ui.detail
+
+import android.os.Bundle
+import android.view.View
+import com.amary.simovies.base.BaseFragment
+import com.amary.simovies.databinding.FragmentDetailBinding
+
+class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate) {
+    override fun initView(view: View, savedInstanceState: Bundle?) {
+        //TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/amary/simovies/presentation/ui/detail/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.amary.simovies.presentation.ui.detail
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
